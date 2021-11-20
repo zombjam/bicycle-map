d32c963f8ec2d9ad1e4afcd74c54bf4c5f3945c3
zombjam <zombjam@users.noreply.github.com>
2021-11-20 10:28:53 +0000

    deploy: 8b953d531943a5def099dee60e78fefbdb6c0372

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Icon } from './Icon';
-export { default as Header } from './Header';
-export { default as PositionButton } from './PositionButton';
-export { default as PinButtons } from './PinButtons';
-export { default as Map } from './Map';
-export { default as SearchVehicles } from './SearchVehicles';
-export { default as SearchRoad } from './SearchRoad';
-export { default as SearchScenicSpot } from './SearchScenicSpot';
--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Home } from './Home';
-export { default as Road } from './Road';
-export { default as Travel } from './Travel';
